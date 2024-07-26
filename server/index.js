f6b9e3e0eb5bc21a165ee33435a7b6ae0eda1e4a
SvshSingh <svshsingh9@gmail.com>
2024-07-26 13:46:52 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -10,12 +10,7 @@ const outfitsRoutes = require("./routes/outfits");
 const path = require("path");
 // const staticPath = path.join(__dirname, "/pictures");
 
-app.use(cors({
-  origin: true,
-  methods: ["GET","POST","PUT","DELETE"],
-  
-  // credentials: true,
-}))
+app.use(cors())
 app.use(fileUpload()); //default filesize is 1mb
 // max size for the request body is 1mb
 app.use(express.json({ limit: "1mb" }));
