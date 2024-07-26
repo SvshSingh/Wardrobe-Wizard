dc0ff0da28684af700d8d5ad5220e9ae71c219b2
SvshSingh <svshsingh9@gmail.com>
2024-07-26 13:53:54 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -10,19 +10,7 @@ const outfitsRoutes = require("./routes/outfits");
 const path = require("path");
 // const staticPath = path.join(__dirname, "/pictures");
 
-app.use(cors())
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next()});
+// app.use(cors())
 app.use(fileUpload()); //default filesize is 1mb
 // max size for the request body is 1mb
 app.use(express.json({ limit: "1mb" }));
