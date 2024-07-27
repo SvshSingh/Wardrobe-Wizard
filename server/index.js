2bec6806e95acbee6502d8b669702b3fe129b5fd
SvshSingh <svshsingh9@gmail.com>
2024-07-27 22:06:32 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -4,13 +4,17 @@ const fileUpload = require("express-fileupload");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const corsOptions = {
+  origin: 'https://wardrobe-wizard.vercel.app', // Allow only this origin
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 const port = process.env.PORT;
 const clothesRoutes = require("./routes/clothes");
 const outfitsRoutes = require("./routes/outfits");
 const path = require("path");
 // const staticPath = path.join(__dirname, "/pictures");
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(fileUpload()); //default filesize is 1mb
 // max size for the request body is 1mb
 app.use(express.json({ limit: "1mb" }));
@@ -19,7 +23,15 @@ app.use("/public", express.static(path.join(__dirname, "../client/public")));
 
 app.use("/api/clothes", clothesRoutes);
 app.use("/api/outfits", outfitsRoutes);
+app.options('/api/clothes', cors(corsOptions)); // Enable pre-flight requests for this route
+
+app.get('/api/clothes', (req, res) => {
+  // Your logic to handle GET request
+  res.status(200).json({ clothes: [] });
+});
+
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}
+    `);
 });
