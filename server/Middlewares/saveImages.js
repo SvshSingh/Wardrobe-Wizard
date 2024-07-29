35a076a9ad5ec09d2a0517376a4ff5e65eaa4942
SvshSingh <svshsingh9@gmail.com>
2024-07-29 19:40:15 +0530

    first commit

--- server/Middlewares/saveImages.js
+++ server/Middlewares/saveImages.js
@@ -3,17 +3,20 @@ const path = require("path");
 
 const saveImages = (req, res, next) => {
   const messages = [];
+  const uploadDir = path.join(__dirname, "../uploads");
+
+  // Create the directory if it doesn't exist
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
+
   const errorOccurred = Object.keys(req.files).some((key) => {
     const file = req.files[key];
-    const filePath = path.join(
-      __dirname,
-      "../uploads",
-      `${key}.${file.mimetype.split("/")[1]}`
-    ); // Save with the correct extension
+    const filePath = path.join(uploadDir, `${key}.${file.mimetype.split("/")[1]}`); // Save with the correct extension
 
     try {
       // Write the file directly instead of converting to base64
-      fs.writeFileSync(path.resolve(filePath), file.data);
+      fs.writeFileSync(filePath, file.data);
       messages.push(`${key} saved as an image file.`);
       return false; // No error, continue processing
     } catch (err) {
@@ -23,7 +26,9 @@ const saveImages = (req, res, next) => {
   });
 
   if (errorOccurred) {
-    res.status(500).send(messages.join("\n"));
+    res.status(500).json({ message: messages.join("\n") });
+  } else {
+    res.status(200).json({ message: messages.join("\n") });
   }
   next();
 };
